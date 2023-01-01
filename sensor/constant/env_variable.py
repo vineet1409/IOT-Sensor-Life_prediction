78d6f51b18728173c42e49b3e339bf6c933df406
vineet srivastava <srivineet93@gmail.com>
2022-12-31 21:31:02 -0600

    updated sensor package

---
+++ sensor/constant/env_variable.py
@@ -0,0 +1,7 @@
+# import urllib
+
+
+MONGODB_URL_KEY = 'mongodb+srv://avnish:XglZZ9OkjjUw74pZ@ineuron-ai-projects.7eh1w4s.mongodb.net/admin?authSource=admin&replicaSet=atlas-okvkrd-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
