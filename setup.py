b324ec77a2173317ea4e1e54b97ce1fae4850454
vineet srivastava <srivineet93@gmail.com>
2022-12-31 20:52:31 -0600

    added project files

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
 from setuptools import find_packages,setup
 from typing import List
 
-REQUIREMENT_FILE_NAME="requirements.txt"
-
-HYPHEN_E_DOT = "-e ."
-
-def get_requirements_list() -> List[str]:
+def get_requirements()->List[str]:
     """
-    Description: This function is going to return list of requirement
-    mention in requirements.txt file
-    return This function is going to return a list which contain name
-    of libraries mentioned in requirements.txt file
+    This function will return list of requirements
     """
-    with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-        if HYPHEN_E_DOT in requirement_list:
-            requirement_list.remove(HYPHEN_E_DOT)
-        return requirement_list
+    requirement_list:List[str] = []
+
+    """
+    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
+    """
+    return requirement_list
 
 setup(
     name="sensor",
     version="0.0.1",
-    author="Vineet Srivastava",
-    author_email="vsriva4@uic.edu",
+    author="ineuron",
+    author_email="avnish@ineuron.ai",
     packages = find_packages(),
-    install_requires=get_requirements_list(),
+    install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
+
