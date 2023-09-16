8c27966b1f9a211a1e05c219b94aa6a7ddaea807
Vineet Srivastava <srivineet93@gmail.com>
2023-09-16 18:55:34 -0500

    Update mongodb.py

--- big-data-pipeline-main/src/database/mongodb.py
+++ big-data-pipeline-main/src/database/mongodb.py
@@ -10,7 +10,7 @@ class MongodbOperation:
     def __init__(self) -> None:
 
         self.client = pymongo.MongoClient(os.getenv('MONGO_DB_URL'),tlsCAFile=ca)
-        self.db_name="ineuron"
+        self.db_name="vineet"
 
     def insert_many(self,collection_name,records:list):
         self.client[self.db_name][collection_name].insert_many(records)
